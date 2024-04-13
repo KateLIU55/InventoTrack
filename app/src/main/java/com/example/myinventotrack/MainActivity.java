ab3dffe43bb2e452b855a7e4c6d80198c8489e70
kate_liu <kaliu@csumb.edu>
2024-04-12 22:41:47 -0700

    added viewBindings, wired up the buttons.

--- app/src/main/java/com/example/myinventotrack/MainActivity.java
+++ app/src/main/java/com/example/myinventotrack/MainActivity.java
@@ -1,6 +1,9 @@
 package com.example.myinventotrack;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -8,18 +11,61 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import com.example.myinventotrack.databinding.ActivityMainBinding;
+
 public class MainActivity extends AppCompatActivity {
+    private ActivityMainBinding binding;
+    //private Log log;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
         setContentView(R.layout.activity_main);
-
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+        
+        binding.logButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                performLogin();
+            }
+        });
+        binding.signUpButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                performSignup();
+            }
         });
     }
+
+    private void performLogin() {
+        try{
+            String username = binding.userNameInputEditText.getText().toString().trim();
+            String password = binding.passwordInputEditText.getText().toString().trim();
+            
+            Log.d("LoginAttempt", "Username: " + username + ", Password: " + password);
+            
+            validateLogin(username, password);
+            
+    } catch (Exception e) {
+            Log.e("LoginError", "Error during login", e);
+            Toast.makeText(this, "Login failed: " + e.getMessage(), Toast.LENGTH_LONG).show();
+        }
+    }
+
+    private void validateLogin(String username, String password) {
+    }
+
+    private void performSignup() {
+        String newUsername = binding.newAccountInputEditText.getText().toString().trim();
+        String newPassword = binding.setPasswordInputEditText.getText().toString().trim();
+        
+        Log.d("SignUpAttemp", "New Username: " + newUsername + ", New Password: " + newPassword);
+        
+        handleSignUp(newUsername, newPassword);
+    }
+
+    private void handleSignUp(String newUsername, String newPassword) {
+    }
+
+
 }
